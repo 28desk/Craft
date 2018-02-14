f811d8b34c28a89d5ed40608a03dad431ba39f1a
Aphichat Panjamanee <panjamanee@gmail.com>
2018-02-14 09:27:42 +0100

    2.6.3009

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb2ebef4bf2f9f6a0c35ab704c2a59631::getLoader();
+return ComposerAutoloaderInit3b928b2be390ac221a678d26612dd4f0::getLoader();
